01bedbff0e8cdb845fcf4412562e4dd6749f1bcc
Christian <christianlance85@gmail.com>
2020-09-29 18:35:02 -0500

    initial react setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import CreateTasker from "./components/Create.component";
+import Taskerr from "./components/Taskerr.component";
+import EditTaskerr from "./components/Edit.Component";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div className="container">
+
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link to="/" className="navbar-brand">Taskerr app!</Link>
+        <div className="collapse nav-collapse">
+          <ul className="navbar-nav mr-auto">
+            <li className="navbar-item">
+              <Link to="/" className="nav-link">Home</Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/create" className="nav-link">Create Tasks</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      
+      <Route path="/" exact component={Taskerr} />
+      <Route path="/edit/:id" component={EditTaskerr} />
+      <Route path="/create" component={CreateTasker} />
     </div>
+
+    </Router>
   );
 }
 
---
+++ src/components/Taskerr.component.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+export default class Taskerr extends Component {
+    render() {
+        return (
+            <div>
+                <p>Welcome to Taskerr!</p>
+            </div>
+        )
+    }
+}
